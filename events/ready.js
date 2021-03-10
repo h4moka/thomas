8e5404c6fb458eef9f8df1ff9031938695fcb393
NIR0 <74272765+NIR0-V@users.noreply.github.com>
2021-03-10 08:56:32 +0200

    Delete ready.js

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = async (client) => {
-  console.log(`[ ${client.user.username} ] is ready`);
-  await client.user.setActivity("Music", {
-    type: "LISTENING",
-  });
-  await client.user.setStatus('idle')
-};
