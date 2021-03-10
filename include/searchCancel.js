57ff4e79ba840697df8d618e9ca31ef68e404eaa
NIR0 <74272765+NIR0-V@users.noreply.github.com>
2021-03-10 08:57:25 +0200

    Delete searchCancel.js

--- include/searchCancel.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports = (client, message, query, tracks) => {
-
-    message.channel.send(new Discord.MessageEmbed().setColor('00e8ff').setTitle(`${client.emotes.error} - You did not provide a valid response ... Please send the command again !`));
-            console.log("NIRO DEVELOMENT")
-};
