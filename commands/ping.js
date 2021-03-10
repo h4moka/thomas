c65ad2b2fb2e0d3c4ce6866371ce484dcd334837
NIR0 <74272765+NIR0-V@users.noreply.github.com>
2021-03-10 08:55:21 +0200

    Delete ping.js

--- commands/ping.js
+++
@@ -1,6 +0,0 @@
-const Discord = require("discord.js")
-
-exports.run = async (client, message) => {
-  message.channel.send(new Discord.MessageEmbed().setDescription(`${client.emotes.success} - Ping : **${client.ws.ping}ms** !`)
-  );
-}
