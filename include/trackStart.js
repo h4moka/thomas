690e1108839033a65b42c6c53eeca3d5011c7d29
NIR0 <74272765+NIR0-V@users.noreply.github.com>
2021-03-10 08:57:01 +0200

    Delete trackStart.js

--- include/trackStart.js
+++
@@ -1,7 +0,0 @@
-const Discord = require("discord.js")
-
-module.exports = (client, message, track) => {
-
-    message.channel.send(new Discord.MessageEmbed().setColor('00e8ff').setTitle(`${client.emotes.music} - Now playing ${track.title} into ${message.member.voice.channel.name} ...`));
-
-};
